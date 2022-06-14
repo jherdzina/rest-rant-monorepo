10a1eccacf706cfafd7a5fa4cacbfdc606b08cf6
Jennifer Herdzina <92278996+jherdzina@users.noreply.github.com>
2022-06-13 22:53:36 -0500

    added authentication & password capabilities

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -15,11 +15,28 @@ function LoginForm() {
 
     const [errorMessage, setErrorMessage] = useState(null)
 
-    async function handleSubmit(e) {
-        e.preventDefault()
-       
+      
+async function handleSubmit(e) {
+    e.preventDefault()
+    const response = await fetch(`http://localhost:5001/authentication/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
 
+    const data = await response.json()
+
+    if (response.status === 200) {
+        setCurrentUser(data.user)
+        history.push(`/`)
+    } else {
+        setErrorMessage(data.message)
     }
+
+    console.log(data)
+}
 
     return (
         <main>
@@ -65,4 +82,7 @@ function LoginForm() {
     )
 }
 
-export default LoginForm
+export default LoginForm  
+  
+
+
