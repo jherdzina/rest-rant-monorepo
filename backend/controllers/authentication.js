dae7947597e28f9cb8aa7d72b4df7ddf254c179d
Jennifer Herdzina <92278996+jherdzina@users.noreply.github.com>
2022-06-15 18:41:47 -0500

    add jwt to authentication

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('jwt')
 
 const { User } = db
  
@@ -15,10 +16,26 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, {id: user.userId })
+        res.json({ user: user, token: result.value })
         console.log(user)
     }
 })
+
+
+router.get('/profile', async (req, res) => {
+    // try {
+    //     // let user = await User.findOne({
+    //     //     where: {
+    //     //         userId:  
+    //     //     }
+    //     })
+    //     res.json(user)
+    // } catch {
+    //     res.json(null)
+    // }
+})
+
 
 
 
